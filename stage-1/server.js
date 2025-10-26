1bd8d0181558355c03e4e29ad6c301c482b1734c
SDorGS <160716052+SDorGS@users.noreply.github.com>
2025-10-26 09:08:15 +0100

    Update server.js

--- stage-1/server.js
+++ stage-1/server.js
@@ -172,6 +172,5 @@ app.delete('/strings/:value', (req, res) => {
   res.status(204).send();
 });
 
-// ✅ Start server
-const PORT = process.env.PORT || 3000;
-app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
+const PORT = process.env.PORT || 8080;
+app.listen(PORT, '0.0.0.0', () => console.log(`Server running on port ${PORT}`));
